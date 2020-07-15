5968806f9c754c4155a4b5a786f482534339bc96
soadtech <fernandojoseropero@gmail.com>
2020-07-15 16:38:38 -0500

    creacion de funciones para obtener datos de firebase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
 import Layout from "../components/layout/Layout";
+import { FirebaseContext } from "../server";
+
 export default function Home() {
+  const [productos, setProductos] = useState([]);
+
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const obtenerProductos = () => {
+      firebase.db
+        .collection("productos")
+        .orderBy("creado", "desc")
+        .onSnapshot(manejarSnapshot);
+    };
+    obtenerProductos();
+  }, []);
+
+  function manejarSnapshot(snapshot) {
+    const productos = snapshot.docs.map((doc) => {
+      return {
+        id: doc.id,
+        ...doc.data(),
+      };
+    });
+
+    setProductos(productos);
+  }
   return (
     <div>
       <Layout>
