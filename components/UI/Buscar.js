171ae1a0d6a5661e467236dc6f5dbc26ed1e9491
soadtech <fernandojoseropero@gmail.com>
2020-07-14 20:44:32 -0500

    se agregaron estilos al header

--- components/UI/Buscar.js
+++ components/UI/Buscar.js
@@ -1,10 +1,41 @@
 import React from "react";
+import styled from "@emotion/styled";
+import { css } from "@emotion/core";
+
+const InputText = styled.input`
+  border: 1px solid var(--gris3);
+  padding: 1rem;
+  min-width: 300px;
+  margin-left: 2rem;
+`;
+const InputSumit = styled.button`
+  height: 3rem;
+  width: 3rem;
+  display: block;
+  background-size: 4rem;
+  background-image: url("/static/img/buscar.png");
+  background-repeat: no-repeat;
+  position: absolute;
+  top: 1px;
+  right: 1rem;
+  background-color: white;
+  border: none;
+  text-indent: -9999px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 const Buscar = () => {
   return (
-    <form action="">
-      <input type="text" />
-      <button type="submit">Buscar</button>
+    <form
+      css={css`
+        position: relative;
+      `}
+    >
+      <InputText type="text" placeholder="Buscar Productos" />
+      <InputSumit type="submit">Buscar</InputSumit>
     </form>
   );
 };
--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,27 +1,84 @@
 import React from "react";
+import Link from "next/link";
+import styled from "@emotion/styled";
+import { css } from "@emotion/core";
 import Buscar from "../UI/Buscar";
 import Navegacion from "./Navegacion";
-import Link from "next/link";
+import Boton from "../UI/Boton";
 
+const ContenedorHeader = styled.div`
+  max-width: 1200px;
+  width: 95%;
+  margin: 0 auto;
+
+  @media (min-width: 768px) {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+
+const Logo = styled.p`
+  color: var(--naranja);
+  font-size: 4rem;
+  line-height: 0;
+  font-weight: 700;
+  font-family: "Roboto Slab", serif;
+  margin-left: 2rem;
+`;
 const Header = () => {
+  const user = true;
   return (
-    <header>
-      <div>
-        <div>
-          <p>P</p>
+    <header
+      css={css`
+        border-bottom: 2px solid var(--gris3);
+        padding: 1rem 0;
+      `}
+    >
+      <ContenedorHeader>
+        <div
+          css={css`
+            display: flex;
+            align-items: center;
+          `}
+        >
+          <Link href="/">
+            <Logo>P</Logo>
+          </Link>
 
           <Buscar />
 
           <Navegacion />
         </div>
 
-        <div>
-          <p>Hola : Fernando</p>
-          <button type="button">Cerrar Sesion</button>
-          <Link href="/">Login</Link>
-          <Link href="/">Crear Cuenta</Link>
+        <div
+          css={css`
+            display: flex;
+            align-items: center;
+          `}
+        >
+          {user ? (
+            <>
+              <p
+                css={css`
+                  margin-right: 2rem;
+                `}
+              >
+                Hola : Fernando
+              </p>
+              <Boton bgColor="true">Cerrar Sesion</Boton>
+            </>
+          ) : (
+            <>
+              <Link href="/">
+                <Boton bgColor="true">Login</Boton>
+              </Link>
+              <Link href="/">
+                <Boton>Crear Cuenta</Boton>
+              </Link>
+            </>
+          )}
         </div>
-      </div>
+      </ContenedorHeader>
     </header>
   );
 };
